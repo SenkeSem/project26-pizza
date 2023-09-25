06f42fa36b0307e117cabc7b5d98b54fcc42fdcb
SenkeSem <sen9morf@gmail.com>
2023-09-25 18:54:45 +0300

    add logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Pizza!</h1>
+    <div className="wrapper">
+      <div className="header">
+        <div className="logo">
+          <img width={38} height={38} src="./img/logo.svg" alt="logo" />
+          <div>
+            <p>REACT PIZZA</p>
+            <p>самая вкусная пицца во вселенной</p>
+          </div>
+        </div>
+        <div className="cart"></div>
+      </div>
     </div>
   );
 }
