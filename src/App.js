3f47a3bff5f6f9402ab664e28980e66c482dace8
SenkeSem <sen9morf@gmail.com>
2023-09-27 09:11:16 +0300

    add categories bar and sort bar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
         <div className="logo">
           <img width={38} height={38} src="./img/logo.svg" alt="logo" />
           <div>
-            <p>REACT PIZZA</p>
+            <h2>REACT PIZZA</h2>
             <p>самая вкусная пицца во вселенной</p>
           </div>
         </div>
@@ -19,6 +19,28 @@ function App() {
         </div>
       </div>
       <div className="header-line"></div>
+
+      <nav className="categories">
+        <ul>
+          <li className="active">Все</li>
+          <li>Мясные</li>
+          <li>Вегетарианская</li>
+          <li>Гриль</li>
+          <li>Острые</li>
+          <li>Закрытые</li>
+        </ul>
+        <div className="sort">
+          <img width={10} height={6} src="./img/triangle.svg" alt="triangle" />
+          <p>Сортировка по:</p>
+          <span>популярности</span>
+        </div>
+        <div className="popup">
+          <p className="active">популярности</p>
+          <p>по цене</p>
+          <p>по алфавиту</p>
+        </div>
+      </nav>
+
     </div>
   );
 }
