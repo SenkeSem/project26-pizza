13977304094d781702360bd4aeadaeaa62d96e06
SenkeSem <sen9morf@gmail.com>
2023-09-27 17:15:12 +0300

    add pizza-block item

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           </div>
         </div>
         <div className="cart">
-          <p className="price">520 руб.</p>
+          <p className="price">520 ₽</p>
           <img width={1} height={25} src="./img/cart-line.svg" alt="cart-line" />
           <div className="cart-block">
             <img src="./img/cart.svg" alt="cart" />
@@ -41,6 +41,33 @@ function App() {
         </div>
       </nav>
 
+      <h1>Все пиццы</h1>
+
+      <div className="pizza-list">
+        <div className="pizza-block">
+          <img width={260} height={260} src="./img/pizza.png" alt="pizza" />
+          <h3>Чизбургер-пицца</h3>
+          <div className="selector">
+            <ul className="dough">
+              <li className="active">тонкое</li>
+              <li>традиционное</li>
+            </ul>
+            <ul className="size">
+              <li className="active">26 см.</li>
+              <li>30 см.</li>
+              <li>40 см.</li>
+            </ul>
+          </div>
+          <div className="button">
+            <h4>от 395 ₽</h4>
+            <div className="add-button">
+              <img src="./img/plus.svg" alt="plus" />
+              <p>Добавить</p>
+              <img src="./img/number.svg" alt="number" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
