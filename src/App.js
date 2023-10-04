0f4d94b0455f5bf40d3e9d8c4385899fd1899dc2
SenkeSem <sen9morf@gmail.com>
2023-10-04 17:33:01 +0300

    conduct a small experiment on working with react toolkit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './redux/slices/filterSlice';
+
 import Home from './pages/Home';
 import Cart from './pages/Cart/Cart';
 import NotFound from './pages/NotFound/NotFound';
@@ -11,8 +14,23 @@ export const SearchContext = React.createContext();
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
 
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="wrapper">
+      {/* <div>
+        <div>
+          <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+            Increment
+          </button>
+          <span>{count}</span>
+          <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+            Decrement
+          </button>
+        </div>
+      </div> */}
+
       <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <Routes>
--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import ReactDOM from 'react-dom/client';
 import './scss/index.scss';
 import App from './App';
 
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
