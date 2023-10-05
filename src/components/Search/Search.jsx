87169aea45ab4215fc5c96ce5aed4c328d21a256
SenkeSem <sen9morf@gmail.com>
2023-10-05 19:39:03 +0300

    make search optimization

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,26 +3,44 @@ import React from 'react';
 import style from './Search.module.scss';
 import { SearchContext } from '../../App';
 
+import debounce from 'lodash.debounce';
+
 function Search() {
-  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const [value, setValue] = React.useState('');
+
+  const { setSearchValue } = React.useContext(SearchContext);
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setValue('');
+    setSearchValue('');
+    inputRef.current.focus();
+  };
+
+  const updateSearchValue = React.useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 500),
+    [],
+  );
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
 
   return (
     <div className={style.container}>
       <img width={20} height={20} src="img/search.png" alt="search" />
       <input
-        value={searchValue}
-        onChange={(event) => setSearchValue(event.target.value)}
+        ref={inputRef}
+        value={value}
+        onChange={onChangeInput}
         type="text"
         placeholder="Введите пиццу..."
       />
-      {searchValue && (
-        <img
-          onClick={() => setSearchValue('')}
-          width={20}
-          height={20}
-          src="img/remove.png"
-          alt="remove"
-        />
+      {value && (
+        <img onClick={onClickClear} width={20} height={20} src="img/remove.png" alt="remove" />
       )}
     </div>
   );
