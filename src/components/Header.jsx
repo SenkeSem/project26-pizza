e921bb28805ba0361c2c4913162195bfd4d0dded
SenkeSem <sen9morf@gmail.com>
2023-10-18 09:51:57 +0300

    Скрывает корзину при переходе на страницу /cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Search from './Search/Search';
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function Header() {
+  const location = useLocation();
+
   const { pizzas, totalPrice } = useSelector((state) => state.cart);
 
   const totalCount = pizzas.reduce((sum, item) => sum + item.counterAdd, 0);
@@ -22,14 +24,16 @@ function Header() {
         </Link>
         <Search />
         <Link to="/cart">
-          <div className="cart">
-            <p className="price">{totalPrice} ₽</p>
-            <img width={1} height={25} src="img/cart-line.svg" alt="cart-line" />
-            <div className="cart-block">
-              <img src="img/cart.svg" alt="cart" />
-              <p>{totalCount}</p>
+          {location.pathname !== '/cart' && (
+            <div className="cart">
+              <p className="price">{totalPrice} ₽</p>
+              <img width={1} height={25} src="img/cart-line.svg" alt="cart-line" />
+              <div className="cart-block">
+                <img src="img/cart.svg" alt="cart" />
+                <p>{totalCount}</p>
+              </div>
             </div>
-          </div>
+          )}
         </Link>
       </div>
       <div className="header-line"></div>
